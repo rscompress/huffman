b368352599b14e21ad7e0138d77fdf60b9709b32
ucyo <cayoglu@me.com>
2020-02-25 22:57:25 +0100

    [lib] Add information about Huffman algorithm

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,13 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
+//! This crate implements the Huffman Coding algorithm specified
+//! in the [master thesis](http://compression.ru/download/articles/huff/huffman_1952_minimum-redundancy-codes.pdf)
+//! of David A. Huffman.
+//!
+//! # Huffman Coding
+//! This implementation uses stream coding and canonical Huffman Coding. The file
+//! is being read in chunks and coded using multi-pass encoding.
+//!
+//! ## Encoding Workflow
+//! The algorithm first traverses the file and builds a histogram for each byte.
+//! Afterwards it builds the codewords using a compact representation of the codewords
+//! described in the above paper. A second traversal of file then encodes each
+//! byte and saves it on disk.

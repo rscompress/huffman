13c27d57cce834730e14188e5aa93d79fc514b8e
ucyo <cayoglu@me.com>
2020-03-18 22:16:37 +0100

    Remove one collect()

--- src/huffman.rs
+++ src/huffman.rs
@@ -62,9 +62,9 @@ pub fn calculate_codeword_length_inplace(histogram: &mut [usize]) {
 
 
 pub fn sort_by_value(store: &[usize]) -> Vec<(u8, usize)> {
-    let mut sorted_tuple: Vec<_> = store.iter().enumerate().filter(|a| *a.1 > 0 as usize).collect();
-    sorted_tuple.sort_by(|a, b| b.1.cmp(a.1));
-    let sorted_tuple = sorted_tuple.iter().map(|(a, b)| (*a as u8, **b)).collect();
+    let mut sorted_tuple: Vec<_> = store.into_iter().enumerate().filter(|(_,b)| **b > 0 as usize).map(|(a,b)| (a as u8, *b)).collect();
+    sorted_tuple.sort_by(|a, b| b.1.cmp(&a.1));
+    // let sorted_tuple = sorted_tuple.into_iter().map(|(a, b)| (a as u8, *b)).collect();
     sorted_tuple
 }
 

88ed596afededa1b16e4f70607a4393138f2d8b2
ucyo <cayoglu@me.com>
2020-03-26 11:23:01 +0100

    Add method to export codewords and lengths to be used by decoding

--- src/model.rs
+++ src/model.rs
@@ -1,4 +1,8 @@
+
+use std::collections::BTreeMap;
+
 pub trait Model {
     fn encode(&self, sym: u8) -> (usize, usize);
     fn sentinel(&self) -> usize;
+    fn to_btreemap(&self) -> BTreeMap<usize, (usize, usize)>;
 }

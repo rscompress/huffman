8c08e0a56aa756ed52d5071e37f50688f5c46032
ucyo <cayoglu@me.com>
2020-03-29 11:19:29 +0200

    Add script for generating testfiles with certain symbol distribution

---
+++ examples/generate_testfiles.rs
@@ -0,0 +1,14 @@
+
+use rscompress_huffman::stats::generate_random_byte_vector;
+use std::fs::File;
+use std::io::{BufRead, BufReader, BufWriter, Write};
+
+fn main() {
+    let fln = "test.raw";
+    let words: Vec<u8> = vec![20, 17, 6, 3, 2, 2, 2, 1, 1, 1];
+    let mut origin: Vec<u8> = generate_random_byte_vector(0, words.len() as u8, 35_044, &words);
+    let dfile = File::create(&fln).expect("Error generating testfile");
+    let mut w = BufWriter::with_capacity(4096, dfile);
+    w.write_all(&origin);
+    println!("Success: {}", fln)
+}

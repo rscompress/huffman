a6638a620d6d2e66ccf1160928289ea187bdf9e2
ucyo <cayoglu@me.com>
2020-05-14 20:37:26 +0200

    Generate problematic files

--- examples/finderrors.rs
+++ examples/finderrors.rs
@@ -1,5 +1,5 @@
 #![allow(unused_imports)]
-use log::info;
+use log::{info, error};
 use rscompress_huffman::huffman::decode::{read, Decoder};
 use rscompress_huffman::huffman::encode::Encoder;
 use rscompress_huffman::huffman::Huffman;
@@ -21,10 +21,10 @@ fn main() {
         histogram[i] = words[i] as usize;
     }
 
-    for j in 0..1 {
+    for j in 0..50 {
         // Generate random data
         let origin: Vec<u8> =
-            generate_random_byte_vector(0, words.len() as u8, 500_000_000, &words);
+            generate_random_byte_vector(0, words.len() as u8, 5_000_000, &words);
         // If error found save to file
         // let dfile = File::create("errors.raw").expect("Failed to create destination file");
         // let mut w = BufWriter::with_capacity(4096, dfile);
@@ -44,12 +44,31 @@ fn main() {
         let mut enc = Encoder::new(Cursor::new(Vec::new()), &h);
         enc.write(&origin).expect("");
         enc.flush().expect("");
-        info!("E {}", now.elapsed().as_secs_f32());
+        info!("Enc {}", now.elapsed().as_secs_f32());
+
+        let encoded_data : Vec<u8> = enc.inner.get_ref().iter().map(|&x| x).collect();
+        // println!("{:?}", encoded_data);
+        // println!("{:?}", data);
+
+        let now = Instant::now();
+        let decoder = rscompress_huffman::huffman::decode::vault::Decoder::new(encoded_data.into_iter(), &h, origin.len() as u64);
+        let decoded_data: Vec<u8> = decoder.collect();
+        info!("Itr {}", now.elapsed().as_secs_f32());
+        if decoded_data != origin {
+            error!("{} ITR wrong!", j);
+            write_file("itr", j, &origin)
+        }
+        // println!("{:?}", decoded_data);
+        // assert_eq!(decoded_data, origin);
 
         // Old decoding method
         let now = Instant::now();
         let decoded_words = read(enc.inner.get_ref(), &h, enc.readbytes);
-        info!("O {}", now.elapsed().as_secs_f32());
+        info!("One {}", now.elapsed().as_secs_f32());
+        if decoded_words != origin {
+            error!("{} ONE wrong!", j);
+            write_file("one", j, &origin)
+        }
 
         // Read decoding method
         let reader = BufReader::new(Cursor::new(enc.inner.get_ref()));
@@ -71,9 +90,28 @@ fn main() {
             // info!("[{},{}] New method looks good", j);
             full.append(&mut buf[..nbytes].to_vec());
         }
-        info!("N {}", now.elapsed().as_secs_f32());
-        assert_eq!(full, origin);
-        assert_eq!(decoded_words, full);
-        info!("{} Success", j)
+        info!("Rea {}", now.elapsed().as_secs_f32());
+        if full != origin {
+            error!("{} REA wrong!", j);
+            write_file("read", j, &origin)
+        }
+
+        // if full == origin && decoded_data == origin {
+        //     info!("{} Success", j)
+        // } else if full != origin && decoded_data != origin {
+        //     info!("{} Both failed", j)
+        // } else if full != origin {
+        //     info!("{} Only ITR failed")
+        // }
+        // assert_eq!(full, origin);
+        // assert_eq!(decoded_words, full);
     }
+}
+
+
+fn write_file(method: &str, run: usize, data: &[u8]) {
+    let file  = format!("testdata/errors.{}.{}.raw", method, run);
+    let dfile = File::create(file).expect("Failed to create destination file");
+    let mut w = BufWriter::with_capacity(4096, dfile);
+    w.write_all(&data).unwrap();
 }
